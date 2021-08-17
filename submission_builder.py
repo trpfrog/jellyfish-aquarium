8a095eb93aa4bf34dd10242bd15948bd8d90480c
TrpFrog <dev@trpfrog.net>
2021-08-17 19:38:49 +0900

    Create submission_builder.py
    Since the submission box only accepts one pde file, I created a script to combine multiple pde files.

---
+++ submission_builder.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+
+
+def make_empty_folder(folder_path:str):
+    if os.path.exists(folder_path):
+        if os.path.isdir(folder_path):
+            shutil.rmtree(folder_path)
+        else:
+            os.remove(folder_path)
+
+    os.mkdir(folder_path)
+
+
+def copy_files(from_path:str, to_path:str, extension:str):
+    files = os.listdir(from_path)
+    for file in files:
+        if file.endswith(extension):
+            shutil.copy(from_path + '/' + file, to_path)
+
+
+def append_src(to_file, from_file: str):
+    with open(from_file, 'r') as f:
+        for line in f:
+            to_file.write(line)
+            
+
+if __name__ == '__main__':
+    project_name = 'viscomp-final'
+    source_folder_name = 'src'
+    src_folder = './' + source_folder_name
+    out_folder = './' + project_name
+
+    make_empty_folder(out_folder)
+
+    for extension in ['png', 'jpg']:
+        copy_files(src_folder, out_folder, extension)
+
+    # combine all pde files into viscomp-final.pde
+    with open(f'{out_folder}/{project_name}.pde', 'w') as f:
+        append_src(f, f'{src_folder}/{source_folder_name}.pde')
+        files = os.listdir(src_folder)
+        for file in files:
+            if file.endswith('.pde') and file != f'{source_folder_name}.pde':
+                f.write('\n')
+                append_src(f, src_folder + '/' + file)
